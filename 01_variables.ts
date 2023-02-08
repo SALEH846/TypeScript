fbf4128fd9694f3a1ff24362ac3cb158d9410700
Muhammad Saleh <ranaasaleh846@gmail.com>
2023-02-08 18:07:45 +0500

    Variables in TypeScript

---
+++ 01_variables.ts
@@ -0,0 +1,15 @@
+// A simple variable that accepts only string as its value
+let greetings: string = "Hello, My name is Muhammad Saleh";
+
+// cannot do that
+// greetings = 22;
+
+// A simple variable that accepts only number as its value
+// let age: number = 22;
+let age = 22; // both are same
+// if type is not provided then TypeScript automatically infers the type from the value
+
+// A simple variable that accepts only boolean as its value
+let married: boolean = false;
+
+console.log(greetings);
