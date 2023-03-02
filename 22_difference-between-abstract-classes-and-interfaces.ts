7fce93d9f5082c9442b5e984a41a49f97e11f8da
Muhammad Saleh <ranaasaleh846@gmail.com>
2023-03-02 15:02:04 +0500

    Difference between Abstract Classes and Interfaces in TypeScript

---
+++ 22_difference-between-abstract-classes-and-interfaces.ts
@@ -0,0 +1,11 @@
+// Interfaces
+// -------------------------------
+// 1. All members are abstract.
+// 2. Interfaces support multiple inheritances.
+// 3. TypeScript Interface has zero JavaScript code that means it is only available in TypeScript and does not produce any code in compiled JavaScript file
+
+// Abstract classes
+// -------------------------------
+// 1. Some members are abstract and some are fully implemented.
+// 2. Abstract class does not support multiple inheritances.
+// 3. Abstract class compile to JavaScript functions.
